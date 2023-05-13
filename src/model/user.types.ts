af95e05eef4e397f3632cce06a67df27140dfafb
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-13 13:41:25 +0500

    feat: add check for user info type

--- src/model/user.types.ts
+++ src/model/user.types.ts
@@ -9,3 +9,14 @@ export type UserRecord = UserInfo & {
 };
 
 export type UserUpdate = Partial<UserInfo>;
+
+export function isUserInfo(info: unknown): info is UserInfo {
+  return (
+    (info as UserInfo).username !== undefined &&
+    typeof (info as UserInfo).username === 'string' &&
+    (info as UserInfo).age !== undefined &&
+    typeof (info as UserInfo).age === 'number' &&
+    (info as UserInfo).hobbies !== undefined &&
+    Array.isArray((info as UserInfo).hobbies)
+  );
+}
