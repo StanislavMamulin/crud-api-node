8ee229271897ccd5bf7b0bbd75736e82e3b84876
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-13 13:48:21 +0500

    feat: add create user method to controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,9 +2,12 @@ import {
   HandleError,
   IdNotFoundError,
   InvalidUrlError,
+  RequiredParametersNotProvided,
+  ServerInternalError,
   UserNotFoundError,
 } from '../Errors/customErrors.js';
 import { USER_API_URL } from '../constants/api.js';
+import { UserInfo, UserRecord, isUserInfo } from '../model/user.types.js';
 import { RouterCallback } from '../routers/router.types';
 import * as UserService from '../services/user.service.js';
 
@@ -44,6 +47,34 @@ export const getUserById: RouterCallback = async (req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'application/json' }).end(JSON.stringify(user));
     res.end();
+  } catch (error) {
+    HandleError(req, res, error);
+  }
+};
+
+export const createUser: RouterCallback = async (req, res) => {
+  try {
+    let body = '';
+
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      try {
+        if (!req.url) throw new InvalidUrlError();
+
+        const userInfo: UserInfo = JSON.parse(body);
+        if (!isUserInfo(userInfo)) throw new RequiredParametersNotProvided();
+
+        const user: UserRecord = UserService.createUser(userInfo);
+        if (!user) throw new ServerInternalError();
+
+        res.writeHead(201, { 'Content-Type': 'application/json' }).end(JSON.stringify(user));
+      } catch (err) {
+        HandleError(req, res, err);
+      }
+    });
   } catch (error) {
     HandleError(req, res, error);
   }
